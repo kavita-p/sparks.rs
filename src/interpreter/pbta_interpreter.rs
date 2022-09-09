0823892cc072fbf9bf2a69f3bd946f66e88acfa2
Kavita <kavi.pdri@gmail.com>
2022-09-08 22:31:35 -0700

    pbta move test #1

--- src/interpreter/pbta_interpreter.rs
+++ src/interpreter/pbta_interpreter.rs
@@ -13,13 +13,17 @@ pub fn pbta_move(rolls: Rolls, stat: i32) -> Reply {
         i32::MIN..=6 => ("Failure!", Failure),
     };
 
-    let mut description = format!("Got {} on 2d6", score);
+    let mut description = format!("Got **{}** on 2d6", score);
 
     match score.cmp(&0) {
         Ordering::Greater => { write!(description, " + {}.", stat).unwrap(); },
         Ordering::Equal => { description += "."; }
         Ordering::Less => { write!(description, " - {}.", stat.saturating_abs()).unwrap(); },
     };
+
+    if score >= 12 {
+        write!(description, "\n\nYou also gain any bonuses that trigger on a **12+** for this move, if applicable.").unwrap();
+    }
 
     Reply {
         title: String::from(title_literal),
@@ -31,10 +35,27 @@ pub fn pbta_move(rolls: Rolls, stat: i32) -> Reply {
 
 #[cfg(test)]
 mod pbta_interpreter_tests {
+    use super::*;
+
 
     #[test]
     fn renders_positive_stat () {
+        let correct_reply = Reply {
+            title: String::from("Full success!"),
+            description: String::from("Got **12** on 2d6 + 0.\n\nYou also gain any bonuses that trigger on a **12+** for this move, if applicable."),
+            status: Crit,
+            dice: vec![6, 6]
+        };
 
+        let rolls = Rolls {
+            max: 6,
+            min: 6,
+            dice: vec![6, 6]
+        };
+
+        let sparks_reply = pbta_move(rolls, 0);
+
+        assert_eq!(sparks_reply, correct_reply);
     }
 }
 
