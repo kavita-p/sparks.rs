a2c0aeaae1db9123b4917a16a613b1ad90f8426c
Kavita <kavi.pdri@gmail.com>
2022-09-13 16:58:34 -0700

    fixed up fitd string formatting

--- src/interpreter/forged_interpreter.rs
+++ src/interpreter/forged_interpreter.rs
@@ -31,7 +31,7 @@ fn forged_dice(rolls: Rolls, roll_type: ForgedType, zero_d: bool) -> Reply {
         }),
         Resist => {
             if status == Crit {
-                String::from("Critical!")
+                String::from("Clear 1 stress!")
             } else {
                 format!("Take **{}** stress to resist.", 6 - score)
             }
@@ -39,46 +39,34 @@ fn forged_dice(rolls: Rolls, roll_type: ForgedType, zero_d: bool) -> Reply {
         Downtime => String::from(match status {
             Crit => "Critical!",
             FullSuccess => "Increased effect!",
-            MixedSuccess => "Standard effect!",
-            Failure => "Reduced effect!",
+            MixedSuccess => "Standard effect.",
+            Failure => "Reduced effect.",
         }),
         Clear => {
             format!("Clear **{}** stress.", score)
         }
     };
 
-    let mut description = String::from("");
-
-    if sixes > 1 {
-        write!(description, "Got **{} sixes** on {}d.", sixes, pool)
-            .expect("write! should return a string.");
+    let mut description = if sixes > 1 {
         match roll_type {
-            Action => description.push_str(" You take **increased effect**."),
-            Resist => description.push_str(" **Clear 1 stress.**"),
-            Downtime => description.push_str(" **5 ticks** on the relevant clock."),
-            Clear => {}
-        };
-    } else {
-        write!(description, "Got **{}** on {}d", score, pool).unwrap_or_default();
-        if zero_d {
-            write!(description, " (rolled as the lower of 2d.)").unwrap_or_default();
-        } else {
-            write!(description, ".").unwrap_or_default();
-        };
-        if roll_type == Downtime {
-            write!(
-                description,
-                " **{} ticks** on the relevant clock.",
-                match status {
-                    Crit => 5,
-                    FullSuccess => 3,
-                    MixedSuccess => 2,
-                    Failure => 1,
-                }
-            )
-            .unwrap_or_default();
+            Action => format!("Got **{sixes} sixes** on {pool}d. You take **increased effect.**"),
+            Resist => format!("Rolled a **critical** to resist. (Got **{}** sixes.)", sixes),
+            Downtime => format!("Extreme effect, or 5 ticks on the relevant clock. Got **{sixes} sixes** on {pool}d."),
+            Clear => format!("Got **{sixes} sixes** on {pool}d. Check if your game has any rules when rolling a crit to clear stress.")
         }
+    } else {
+        format!("Got {score} on {pool}d")
     };
+
+    if zero_d {
+        description.push_str(" (rolled as the lowest of 2d.)")
+    } else if sixes < 2 {
+        description.push_str(".")
+    };
+
+    if roll_type == Clear {
+        description.push_str("\n\nIf this is more stress then you currently have, you **overindulge.**")
+    }
 
     Reply {
         title,
@@ -96,7 +84,7 @@ mod tests {
     fn action_crit () {
         let correct_reply = Reply {
             title: "Critical success!".to_string(),
-            description: "Got **2 sixes** on 3d. You take **increased effect**.".to_string(),
+            description: "Got **2 sixes** on 3d. You take **increased effect.**".to_string(),
             status: Crit,
             dice: vec![6, 2, 6]
         };
@@ -111,4 +99,4 @@ mod tests {
 
         assert_eq!(correct_reply, sparks_reply);
     }
-} 
+}
